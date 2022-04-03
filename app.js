147ba30fabb1765b141e75fb9bc9ac9a980209d9
Venkatesh Anandan <venkatesha04offcl@gmail.com>
2022-04-03 23:13:03 +0530

    gitpush

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile("/index.html");
+  res.sendFile("/ssologin.html");
 });
 
-app.get("/signup", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/ssologin.html`));
-});
+// app.get("/signup", (req, res) => {
+//   res.sendFile(path.join(`${__dirname}/public/ssologin.html`));
+// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("App is listening on port 3000");
